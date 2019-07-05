2240832e27c40bf550c8fe5f65ee0db87d5e248a
PG-8 <40054342+PG-8@users.noreply.github.com>
2019-07-05 10:45:54 +0200

    changed a few references in scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './src/App.js'
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> f790b79533f948e72d292ef7af373c687020c599
