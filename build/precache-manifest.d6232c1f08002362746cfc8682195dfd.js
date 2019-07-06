54e312ed1c34d4cd723000759889aaf273224077
PG-8 <40054342+PG-8@users.noreply.github.com>
2019-07-06 11:03:50 +0200

    pushing optimized build

---
+++ build/precache-manifest.d6232c1f08002362746cfc8682195dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2663a7b1fd18cd4883bab9e682b731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a0424b8ed5e2442115",
+    "url": "/static/css/main.c2d6c0b4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "59a0424b8ed5e2442115",
+    "url": "/static/js/main.5f8f617e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
